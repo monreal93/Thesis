d2e33466315dc7de3846442560b17428c5ab2259
Alejandro <alejandromonreal93@outlook.com>
2020-02-04 13:53:20 +0100

    Code clean up

--- Undersampling_kspace.m
+++ Undersampling_kspace.m
@@ -1,6 +1,15 @@
 addpath(genpath('/home/amonreal/Documents/Thesis/Matlab_scripts/Code4Alejandro/shepp_logan3d'))
 
-%%% Code to use when a NIFTI is used.
+%% Tasks
+%   - How to properly define del_ky and del_kz, line 68-71
+%   - How to 
+% 
+% 
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%  For NIFTI
+% %%% User input:
 % file = 'maria.nii.gz';
 % % file = 'sub-OAS30001_ses-d0757_run-01_T1w.nii.gz';
 % i_t = niftiread(file); info = niftiinfo(file);
@@ -14,37 +23,33 @@ addpath(genpath('/home/amonreal/Documents/Thesis/Matlab_scripts/Code4Alejandro/s
 % p_bw = 70;
 % Ry = 4; Rz = 2;
 % k_fov = 1./p_s;
-%%%%%%%%%%%%%%%%%%%%%%%
+% gamma = 42.58e6;                                                                % Gyromagnetic ratio
+% caipi = 1;                                                                               % 1 to dephase pair lines as in 2D CAIPI
 
-%%% Using a Phantom....
+%% For Phantom
+%%% User input:
 i_t = phantom3d(240);
-i_f = fftshift(fftn(i_t));
-i_fov = [size(i_t,1) size(i_t,2) size(i_t,3)];                                   % mm3
-p_s = [1e-3 1e-3 1e-3];
-voxel_size = p_s(1)*p_s(2)*p_s(3)*.001;
-x_points = i_fov(1)*6;
-gy_gz_amplit = 6e-3;
-sins = 7;
-p_bw = 70;
-Ry = 3; Rz = 3;
-k_fov = 1./p_s;
-%%%%%%%%%%%%%
+i_f = fftshift(fftn(i_t));                                                                  
+i_fov = [size(i_t,1) size(i_t,2) size(i_t,3)];                                % Vector with fov, [x y z]
+p_s = [1e-3 1e-3 1e-3];                                                         % Vector with pixel size [x y z]
+voxel_size = p_s(1)*p_s(2)*p_s(3)*.001;                                % voxel size mm3
+x_points = i_fov(1)*6;                                                             % number 6 if for oversampling, change if needed
+gy_gz_amplit = 6e-3;                                                              % Max amplitude of sin readout gradients
+sins = 7;                                                                                % # of sins per readout line      
+p_bw = 70;                                                                             % pixel BW
+Ry = 3; Rz = 3;                                                                       % Undersampling factor      
+k_fov = 1./p_s;                                                                        % K-space FOV
+gamma = 42.58e6;                                                                % Gyromagnetic ratio
+caipi = 1;                                                                               % 1 to dephase pair lines as in 2D CAIPI
+plt = 0;                                                                                   % 1 to plot all trajectories
 
+%% Calculating parameters for Gradients
 t_r = 1/p_bw;
 delta_t = 1e-7;
 t_prime = delta_t:delta_t:t_r;
 t = 0+(t_r/x_points):t_r/x_points:t_r;
 f = sins/t_r;
-caipi = 1;                % 1 to dephase pair lines as in 2D CAIPI
-gamma = 42.58e6;
-
-del_ky = k_fov(2)/(i_fov(2)/Ry);
-del_kz = k_fov(3)/(i_fov(3)/Rz);
-t_r = 1/p_bw;
 omg = 2*pi*f;
-
-kx = zeros(round(k_fov(2)/del_ky),round(k_fov(3)/del_kz),x_points);
-ky =kx; kz = kx;
 
 x_calc_points = length(t_prime);
 x_calc = ((-k_fov(1)/2)+(k_fov(1)/x_calc_points)):(k_fov(1))/x_calc_points:(k_fov(1)/2);
@@ -60,10 +65,17 @@ z =  interp1(t_prime,z_calc,t)+range_z;
 figure; plot3(x,y,z)
 % %%%%
 
-del_ky = (k_fov(2)/(i_fov(2)/Ry))+(max(y)-min(y));
-del_kz = (k_fov(3)/(i_fov(3)/Rz))+(max(z)-min(z));
+del_ky = k_fov(2)/(i_fov(2)/Ry);
+del_kz = k_fov(3)/(i_fov(3)/Rz);
+% del_ky = (k_fov(2)/(i_fov(2)/Ry))+(max(y)-min(y));
+% del_kz = (k_fov(3)/(i_fov(3)/Rz))+(max(z)-min(z));
 original_y = y; 
 
+%%% Creating null matrices for K points
+kx = zeros(round(k_fov(2)/del_ky),round(k_fov(3)/del_kz),x_points);
+ky =kx; kz = kx;
+
+%%  Generating K-space trajectories
 for j=1:(k_fov(3)/del_kz)
     for i=1:(k_fov(2)/del_ky)
             kx(i,j,:) = x;
@@ -84,19 +96,21 @@ for j=1:(k_fov(3)/del_kz)
       end
 end
 
-%%% ploting all the trajerctories
-for j = 1:k_fov/del_ky    
-    xx = squeeze(kx(j,:,:));
-    yy = squeeze(ky(j,:,:));
-    zz = squeeze(kz(j,:,:));
-    for i=1:k_fov/del_kz
-        plot3(xx(i,:),yy(i,:),zz(i,:));
-        hold on
+%%  Ploting all the trajerctories
+if plt == 1
+    for j = 1:k_fov/del_ky    
+        xx = squeeze(kx(j,:,:));
+        yy = squeeze(ky(j,:,:));
+        zz = squeeze(kz(j,:,:));
+        for i=1:k_fov/del_kz
+            plot3(xx(i,:),yy(i,:),zz(i,:));
+            hold on
+        end
     end
+    xlabel('Kx');ylabel('Ky');zlabel('Kz');view(3)
 end
-xlabel('Kx');ylabel('Ky');zlabel('Kz');view(3)
 
-% %%% Interpolating
+%%  Interpolating
 % kx = kx(:);
 % ky = ky(:);
 % kz = kz(:);
@@ -119,4 +133,4 @@ xlabel('Kx');ylabel('Ky');zlabel('Kz');view(3)
 % 
 % as(Interpolated_kspace)
 % as(u_img)
-
+%}
